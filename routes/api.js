24ae35e86e6533d5e796cb132b0d20ddfc1b989d
deborah <danen@kimo.com>
2024-06-07 10:38:01 +0800

    feat: API examples

---
+++ routes/api.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+// 自行建立的 data
+const data = [{ id: 1, name: "小明" }];
+// 設置請求路徑為 /products 請求方法為 get
+router.get("/products", function (req, res, next) {
+  // 使用 res.send() 方法設置響應傳送 { success: true, data }
+  // 使用 res.end() 方法結束響應 
+  // 這裏可以直接在 send() 後 .end() 也可以換行寫 res.end()
+  res.send({ success: true, data }).end();
+});
+
+// 設置請求路徑為 /product 請求方法為 post
+router.post("/product", function (req, res) {
+  // req.body 是前端傳來的資料
+  const product = req.body;
+  // 後端要處理新增 id
+  product.id = new Date().getTime();
+  // 把資料加到 data 中
+  data.push(product);
+  // 傳響應告訴前端已新增成功
+  res.send({ success: true, data }).end();
+  // console.log 看一下 data, 確認是否新增成功
+  console.log(data);
+});
+
+// 設置請求路徑為 /product/:id 請求方法為 delete
+router.delete("/product/:id", function (req, res) {
+  // 首先通過 req.params.id 獲取傳入的 id
+  const id = req.params.id;
+  // 然後遍歷 data 找出 id 符合的那筆資料 將它刪除
+  data.forEach((item, key) => {
+    if (item.id == id) {
+      data.splice(key, 1);
+    }
+  });
+  // 最後傳送響應告訴前端已刪除成功
+  res.send({ success: true, data }).end();
+});
+
+module.exports = router;
